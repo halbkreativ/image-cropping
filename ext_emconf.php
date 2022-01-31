b19a6627186af81d9e2ad2f789e510e51c58f4a8
Christoph Schwob <christoph@websailor.at>
2022-01-31 13:28:48 +0100

    [TASK] init with version 1.3.1 from TER

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "image_crop".
+ *
+ * Auto generated 29-09-2014 15:35
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'cropping for tt_content images',
+	'description' => 'Adds simple cropping options to CType "textpic" and "image"',
+	'category' => 'be',
+	'version' => '1.3.1',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearcacheonload' => 0,
+	'author' => 'Sigfried Arnold',
+	'author_email' => 's.arnold@go-west.at',
+	'author_company' => 'GO.WEST Communications GmbH',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.2.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"7ed0";s:21:"ext_conf_template.txt";s:4:"d1c2";s:12:"ext_icon.gif";s:4:"431a";s:14:"ext_tables.php";s:4:"7aa8";s:14:"ext_tables.sql";s:4:"3d75";s:24:"ext_typoscript_setup.txt";s:4:"0c3a";s:16:"locallang_db.xml";s:4:"0584";s:14:"doc/manual.sxw";s:4:"f88d";s:30:"pi1/class.tx_imagecrop_pi1.php";s:4:"7094";s:14:"res/center.gif";s:4:"7736";s:12:"res/east.gif";s:4:"05e0";s:13:"res/north.gif";s:4:"3e1b";s:17:"res/northeast.gif";s:4:"6d52";s:17:"res/northwest.gif";s:4:"63d9";s:13:"res/south.gif";s:4:"c0ce";s:17:"res/southeast.gif";s:4:"b7f9";s:17:"res/southwest.gif";s:4:"698b";s:12:"res/west.gif";s:4:"53e1";}',
+);
+
